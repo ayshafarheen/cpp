7b55738bfe8c32255f72ba2c771b90ca68e24fb6
ayshafarheen <64208210+ayshafarheen@users.noreply.github.com>
2024-04-03 23:36:12 +0400

    Add files via upload

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afarheen <afarheen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fathmanazmeen <fathmanazmeen@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 10:11:55 by afarheen          #+#    #+#             */
-/*   Updated: 2024/04/02 13:07:05 by afarheen         ###   ########.fr       */
+/*   Updated: 2024/04/03 21:35:36 by fathmanazme      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,66 @@ PmergeMe & PmergeMe::operator=(PmergeMe & obj)
 	return *this;
 }
 
-void PmergeMe::vec_sort()
+void PmergeMe::list_sort()
 {
 
 }
 
-void PmergeMe::list_sort()
+void PmergeMe::vec_sort()
 {
-	
+    std::vector<int>::iterator it, it2;
+    std::vector<int> a;
+    std::vector<int> b;
+    for(it = vec.begin(); it != vec.end(); it += 2)
+    {
+        if(it + 1 != vec.end())
+        {
+            if((*it) > *(it + 1))
+                std::swap(*it, *(it+1));
+        }
+        else
+            break;
+    }
+    for(it = vec.begin(); it != vec.end(); it += 2)
+    {
+        if(it + 1 != vec.end())
+        {
+            if((*it) > *(it + 1))
+                std::swap(*it, *(it+1));
+        }
+        else
+            break;
+    }
+    // for(it = vec.begin(); it != vec.end(); it+=2)
+    // {
+    //     if((it + 1) == vec.end() && vec.size() % 2 != 0)
+    //     {
+    //         b.push_back(*(it));
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         b.push_back(*(it+1));
+    //         a.push_back(*it);
+    //     }
+    // }
+    // b.insert(a.begin(),3);
+    // for(it = b.begin(); it != b.end(); it++)
+    // {
+    //     for(it2 = b.begin(); it != b.end(); it2++)
+    //     {
+    //         if((*it) > *(it + 1))
+    //     }
+    // }
+    // for(it = a.begin(); it != a.end(); it++)
+    // {
+    //     std::cout << *it << " ";
+    // }
+    // std::cout << "\n";
+    // for(it = b.begin(); it != b.end(); it++)
+    // {
+    //     std::cout << *it << " ";
+    // }
 }
 
 void PmergeMe::check_args(char **args)
@@ -66,7 +118,7 @@ void PmergeMe::check_args(char **args)
 		else
 			throw(std::exception());
 		i++;
-	vec_sort();
-	list_sort();
+	// list_sort();
 	}
+	vec_sort();
 }
